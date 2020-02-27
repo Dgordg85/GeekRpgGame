99a482155aad51abd54befb5154417f17f9bf85c
Alexandr K <Dgordg85@gmail.com>
2020-02-27 12:51:05 +0300

    Create Project

---
+++ desktop/src/com/geekbrains/rpg/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.geekbrains.rpg.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.geekbrains.rpg.game.GeekRpgGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GeekRpgGame(), config);
+	}
+}
