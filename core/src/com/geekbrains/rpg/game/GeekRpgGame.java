88eb1c536dd93c7979862f8c0529c462dd191a60
Alexandr K <Dgordg85@gmail.com>
2020-02-28 12:18:06 +0300

    LessonWork

--- core/src/com/geekbrains/rpg/game/GeekRpgGame.java
+++ core/src/com/geekbrains/rpg/game/GeekRpgGame.java
@@ -5,29 +5,60 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class GeekRpgGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Hero hero;
+	private Texture textureGrass;
+	private Texture texturePointer;
+	private Vector2 pointerPosition;
+	private float rt;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		hero = new Hero();
+		textureGrass = new Texture("grass.jpg");
+		texturePointer = new Texture("pointer.png");
+		pointerPosition = new Vector2(0,0);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		float dt = Gdx.graphics.getDeltaTime();
+		update(dt);
+
+		Gdx.gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		drawGrass();
+		batch.draw(texturePointer, pointerPosition.x - 32, pointerPosition.y - 32, 32,32, 64, 64, 1, 1, rt, 0, 0,64,64, false, false);
+		hero.render(batch);
 		batch.end();
+	}
+
+	public void update(float dt){
+		rt -= dt * 90;
+		hero.update(dt);
+		if (Gdx.input.justTouched()){
+			pointerPosition.set(Gdx.input.getX(), 720 - Gdx.input.getY());
+		}
+	}
+
+	public void drawGrass(){
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 5; j++) {
+				this.batch.draw(textureGrass, i * 150, j * 150);
+			}
+		}
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
+
+
 }
--- desktop/src/com/geekbrains/rpg/game/desktop/DesktopLauncher.java
+++ desktop/src/com/geekbrains/rpg/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.geekbrains.rpg.game.GeekRpgGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		config.foregroundFPS = 60;
 		new LwjglApplication(new GeekRpgGame(), config);
 	}
 }
